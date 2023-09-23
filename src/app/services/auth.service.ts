68a077c2097389396e3d582eaaaf3b39d9e64fa2
Azar Ansar <58215644+W4bbiT@users.noreply.github.com>
2023-09-23 12:31:55 -0500

    Update auth.service.ts

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-const AUTH_API = `https://azar-backend.onrender.com/api/user/`
+const AUTH_API = `https://azar-backend.onrender.com/api/user`
 
 const httpOptions = {
   headers: new HttpHeaders({

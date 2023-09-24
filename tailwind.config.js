dfe8f814b17d7f56b9da2e77cfdb3d78439971a2
Azar Ansar <58215644+W4bbiT@users.noreply.github.com>
2023-09-24 03:14:15 +0000

    tailwind ?

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: [
+    "./src/**/*.{html,ts}", // add this line
+  ],
   theme: {
     extend: {
       colors: {
@@ -10,9 +12,5 @@ module.exports = {
       }
     },
   },
-  plugins: [{
-    'postcss-import': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  }],
+  plugins: [],
 };
